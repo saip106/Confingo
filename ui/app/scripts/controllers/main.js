1e8a9c015dee36bc65ff3a5a345e1e3eba3304d6
Sai Gudigundla <saip106@msn.com>
2015-12-06 12:00:36 -0500

    added angular app

---
+++ ui/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name confingoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the confingoApp
+ */
+angular.module('confingoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
